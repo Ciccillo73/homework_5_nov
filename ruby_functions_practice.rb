571bda9831dc177f5ce5c1d70d9d199d509a1792
Francesco <med.dama@gmail.com>
2019-11-05 21:46:18 +0000

    Join_string test added

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -22,14 +22,14 @@ def length_of_string(string)
   return string.length
 end
 
-def join_string(string_1,string_2)
+ def join_string(string_1,string_2)
   joined_string = string_1 + string_2
-return
-# end
-#
-# def add_string_as_number(string_1,string_2)
-#   return "string_1".to_i() + "string_2".to_i()
-# end
+ return
+ end
+
+def add_string_as_number(string_1,string_2)
+ return "string_1".to_i() + "string_2".to_i()
+end
 #
 # def number_to_full_name_month_1(m)
 #   require 'date'
